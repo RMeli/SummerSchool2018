b18d95fae248ea5decf41b1e2a72f9dd46f451a7
Rocco Meli <rocco.meli@bristol.ac.uk>
2018-07-18 14:19:19 +0200

    miniapp gpu copy

--- miniapp/cuda/data.h
+++ miniapp/cuda/data.h
@@ -116,10 +116,16 @@ class Field {
     // TODO : implement the body of update_host() and update_device()
     void update_host() {
         // TODO: copy contents of device_ptr_ to host_ptr_
+	auto size_in_bytes = xdim_ * ydim_ * sizeof(double);
+	auto status = cudaMemcpy(host_ptr_, device_ptr_, size_in_bytes, cudaMemcpyDeviceToHost);
+	cuda_check_status(status);	
     }
 
     void update_device() {
-        // TODO: copy contents of host_ptr_ to device_ptr_ 
+        // TODO: copy contents of host_ptr_ to device_ptr_
+	auto size_in_bytes = xdim_ * ydim_ * sizeof(double);
+        auto status = cudaMemcpy(device_ptr_, host_ptr_, size_in_bytes, cudaMemcpyHostToDevice);
+        cuda_check_status(status);
     }
 
     private:
